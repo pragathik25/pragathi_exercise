70a2c65356a056576b60ff87d3e6014fb8d76bb7
pragathik25 <pkshetty4446@gmail.com>
2023-06-01 15:30:11 +0530

    custom module

--- src/Plugin/Block/ConfigRender.php
+++ src/Plugin/Block/ConfigRender.php
@@ -8,19 +8,18 @@ use Drupal\Core\Block\BlockBase; //using this as base class for the block
 /**
  * Provides a simple block for custom form
  * @Block(
- * id="custom_form_example",
- * admin_label="custom form block",
+ * id="config_form_example",
+ * admin_label="config form block",
  * )
  */
 
-class CustomRender extends BlockBase {
+class ConfigRender extends BlockBase { //extending the base class
     /**
      * @inheritDoc
      */
 
     public function build(){
-        $form = \Drupal::formBuilder()->getForm('Drupal\pragathi_exercise\Form\CustomDemo'); //this service will return customform in the block
+        $form= \Drupal::formBuilder()->getForm('Drupal\pragathi_exercise\Form\ConfigDemo'); //this service will return config form in the block
         return $form;
         }
-
     }
