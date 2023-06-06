69bf0251507f90d4b93dd709c2d4152010c224bf
pragathik25 <pkshetty4446@gmail.com>
2023-06-06 13:18:01 +0530

    custom module

--- src/Commands/DrushCommandsDemo.php
+++ src/Commands/DrushCommandsDemo.php
@@ -1,45 +1,47 @@
 <?php
 
-namespace Drupal\pragathi_exercise\Commands;
+namespace Drupal\pragathi_exercise\Commands;  //namespace
 
-use Consolidation\OutputFormatters\StructuredData\RowsOfFields;
-use Drush\Commands\DrushCommands;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Consolidation\OutputFormatters\StructuredData\RowsOfFields; //to format the output usingRowsOfFields
+use Drush\Commands\DrushCommands; //used as base class
+use Drupal\Core\Entity\EntityTypeManagerInterface; //to use the service
 
-class DrushCommandsDemo extends DrushCommands {
+class DrushCommandsDemo extends DrushCommands {  //extending the base class
+  /**
+   * @var Drupal\Core\Entity\EntityTypeManagerInterface $entityManager
+   *    Entity manager service.
+   */
 
+   public function __construct(EntityTypeManagerInterface $entityTypeManager) {
+    $this->entityManager = $entityTypeManager;  //to load the entity
+    parent::__construct();
+  }
 /**
    * Command that returns a list of all blocked users.
    *
    * @field-labels
-   *  id: User Id
-   *  name: User Name
-   *  email: User Email
-   * @default-fields id,name,email
+   *  id: Node Id
+   *  title: Title
+   * @default-fields id,title
    *
-   * @usage drush-helpers:active-editor
-   *   Returns all active-editor
+   * @usage drush-helpers: get-title
+   *   Returns all get-title
    *
-   * @command drush-helpers:active-editor
-   * @aliases active-editor
+   * @command drush-helpers:get-title
+   * @aliases get-title
    *
    * @return \Consolidation\OutputFormatters\StructuredData\RowsOfFields
    */
-public function blockedUsers() {
-
-    $users = $this->entityManager->getStorage('user')->loadByProperties(['status' => 1]);
+  public function drushDemo() {
+    $nodes = \Drupal::entityTypeManager()->getStorage('node')->loadByProperties(['type' => 'page']); //it will load nodes of type basic page
     $rows = [];
-    foreach ($users as $user) {
-        if ($user->hasRole('content_editor')) {
+    foreach($nodes as $node) {
         $rows[] = [
-            'id' => $user->id(),
-            'name' => $user->name->value,
-            'email' => $user->mail->value,
+          'id' => $node->id(), //returns node id
+          'title' => $node->getTitle(), //returns the title
         ];
-        }
     }
-   // print_r($rows);exit;
-    return new RowsOfFields($rows);
+        return new RowsOfFields($rows); //output will be displayed in rows
     }
+  }
 
-}
