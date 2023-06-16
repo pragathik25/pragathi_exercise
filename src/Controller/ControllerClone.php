4f35a16c10f7f21e17c9a46abebf3086da1bb297
pragathik25 <pkshetty4446@gmail.com>
2023-06-16 17:49:30 +0530

    custom module

---
+++ src/Controller/ControllerClone.php
@@ -0,0 +1,44 @@
+<?php
+namespace Drupal\pragathi_exercise\Controller;
+
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\node\Entity\Node;
+use Drupal\Core\Session\AccountInterface;
+
+class ControllerClone extends ControllerBase {
+
+  public function nodeTitle(Node $node) {
+    if (!empty($node)) {
+        $title = $node->getTitle();
+        return  [
+            '#markup'=>$title,
+        ];
+      }
+      else {
+        throw new NotFoundHttpException();
+      }
+    }
+
+  public function nodeTitlePageTitle(Node $node) {
+  $prepend_text = "Node of ";
+    return $prepend_text . $node->getTitle();
+  }
+
+
+public function accessNode(AccountInterface $account, $node) {
+    $node = Node::load($node);
+    $type = $node-> getType();
+    if ($type == 'article' || $type === 'page') {
+      $result = AccessResult::allowed();
+    }
+    else {
+      $result = AccessResult::forbidden();
+    }
+
+    $result->addCacheableDependency($node);
+
+    return $result;
+  }
+
+}
